4050fc5e927c18962dbe21076e45a565e01d0612
Dominik Suski <dominik.suski@microsoft.wsei.edu.pl>
2021-02-10 20:16:08 +0100

    pobieranie danych do comboboxa z bazy

--- CompanyMaster/AddPage.xaml.cs
+++ CompanyMaster/AddPage.xaml.cs
@@ -22,31 +22,134 @@ namespace CompanyMaster
     /// </summary>
     public partial class AddPg : Page
     {
+        //CompaniesDatabaseEntities dc = new CompaniesDatabaseEntities();
         public AddPg()
         {
             InitializeComponent();
-            using (SqlConnection conn = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;initial catalog=CompaniesDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"))
+            fillComboBox();
+            fillComboBoxBS();
+            fillComboBoxTp();
+            //bindCountry();
+            //bindBS();
+            //bindTps();
+            //using (SqlConnection conn = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;initial catalog=CompaniesDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework"))
+            //{
+            //    try
+            //    {
+            //        string query = "select Country from Countries";
+            //        SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            //        conn.Open();
+            //        DataSet ds = new DataSet();
+            //        da.Fill(ds, "Country");
+            //        //CountryBox.DisplayMember = "Country";
+            //        //CountryBox.ValueMember = "Country";
+            //        //CityBox.DataSource = ds.Tables["Country"];
+
+
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        // write exception info to log or anything else
+            //        MessageBox.Show("Error occured!");
+            //    }
+            //}
+        }
+        void fillComboBox()
+        {
+            SqlConnection conn = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;initial catalog=CompaniesDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            try
             {
-                try
+                conn.Open();
+                string query = "select * from Countries";
+                SqlCommand createCommand = new SqlCommand(query, conn);
+                SqlDataReader dr = createCommand.ExecuteReader();
+                while (dr.Read())
                 {
-                    string query = "select Country from Countries";
-                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
-                    conn.Open();
-                    DataSet ds = new DataSet();
-                    da.Fill(ds, "Country");
-                    //CountryBox.DisplayMember = "Country";
-                    //CountryBox.ValueMember = "Country";
-                    //CityBox.DataSource = ds.Tables["Country"];
-
-
+                    string Country = dr.GetString(1);
+                    CountryBox.Items.Add(Country);
                 }
-                catch (Exception ex)
-                {
-                    // write exception info to log or anything else
-                    MessageBox.Show("Error occured!");
-                }
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
-        
+        void fillComboBoxBS()
+        {
+            SqlConnection conn1 = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;initial catalog=CompaniesDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            try
+            {
+                conn1.Open();
+                string query1 = "select * from BusinessSegments";
+                SqlCommand createCommand1 = new SqlCommand(query1, conn1);
+                SqlDataReader dr1 = createCommand1.ExecuteReader();
+                while (dr1.Read())
+                {
+                    string BusinessSegment = dr1.GetString(1);
+                    SegmentBox.Items.Add(BusinessSegment);
+                }
+                conn1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        void fillComboBoxTp()
+        {
+            SqlConnection conn2 = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;initial catalog=CompaniesDatabase;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            try
+            {
+                conn2.Open();
+                string query2 = "select * from Types";
+                SqlCommand createCommand2 = new SqlCommand(query2, conn2);
+                SqlDataReader dr2 = createCommand2.ExecuteReader();
+                while (dr2.Read())
+                {
+                    string Type = dr2.GetString(1);
+                    TypeBox.Items.Add(Type);
+                }
+                conn2.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //public List<Countries> Coun { get; set; }
+        //public List<BusinessSegments> BS { get; set; }
+        ////public List<Type> Tps { get; set; }
+
+        ////private void bindTps()
+        ////{
+        ////    //throw new NotImplementedException();
+        ////    //CompaniesDatabaseEntities dc = new CompaniesDatabaseEntities();
+        ////    //var item = dc.Types.ToList();
+        ////    //Tps = item;
+        ////    //DataContext = Tps;
+
+        ////}
+
+        //private void bindBS()
+        //{
+        //    //CompaniesDatabaseEntities dc = new CompaniesDatabaseEntities();
+        //    var item = dc.BusinessSegments.ToList();
+        //    BS = item;
+        //    DataContext = BS;
+        //}
+
+
+
+        //private void bindCountry()
+        //{
+        //    //throw new NotImplementedException();
+        //    //CompaniesDatabaseEntities dc = new CompaniesDatabaseEntities();
+        //    var item = dc.Countries.ToList();
+        //    Coun = item;
+        //    DataContext = Coun;
+        //}
+
+
     }
 }
